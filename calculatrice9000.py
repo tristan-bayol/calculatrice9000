ddeeb4760820fc7d18b1ca686bf20b7cae334448
Tristan Bayol <tristan-bayol@laplateforme.io>
2023-11-29 08:05:23 +0100

    Latest Version

--- calculatrice9000.py
+++ calculatrice9000.py
@@ -2,34 +2,30 @@ list_operation = ["+", "-", "*","/", "//", "%","**"]
 list_multiplication = ["*","x","X"]
 list_puissance = ["**","xx","XX"]
 
+#Demande le nombre et verifie que la valeur saisie par l'utilisateur est bien un nombre
+def demander_nombre():
+    chiffre = 0
+    while chiffre == 0:
+        nombre= input("Entrez un nombre ?: ")
+         # Remplace automatiquement les virgules par des points
+        nombre = nombre.replace(",", ".")
+        try:
+            chiffre = float(nombre) + 1
+        except:
+            print("Erreur: Veuillez rentrer un nombre valide !")
+    return float (nombre)
+
+#Demande l'opération a réaliser
+def demander_calcul():
+    calcul = input ("Quelle type d'opération souhaitez-vous effectuer ? : ")    
+    while calcul not in list_operation and calcul not in list_multiplication and calcul not in list_puissance:
+        print ("Erreur, veuillez saisir une opération valide")
+        calcul = input("Quelle type d'opération souhaitez-vous effectuer ? : ")
+    return calcul
+
 while True: #permet de relancer le programe directement apres qu'il ai trouver un resultat !
     
     print("Je suis une calculatrice 9000, et je suis prête à tout calculer pour toi!")
-    
-    #Demande le nombre et verifie que la valeur saisie par l'utilisateur est bien un nombre
-    def demander_nombre():
-        chiffre = 0
-        while chiffre == 0:
-            nombre= input("Entrez un nombre ?: ")
-            try:        
-                chiffre= float(nombre) +1
-            except:
-                if "," in nombre:
-                    print ("Erreur : Veuillez remplacer la , par un .")
-                else: 
-
-                    print ("Erreur: Veuillez rentrer un nombre !")
-        return float (nombre)
-
-    #Demande l'opération a réaliser
-    def demander_calcul():
-        calcul = input ("Quelle type d'opération souhaitez-vous effectuer ? : ")    
-        while calcul not in list_operation and calcul not in list_multiplication and calcul not in list_puissance:
-            print ("Erreur, veuillez saisir une opération valide")
-            calcul = input("Quelle type d'opération souhaitez-vous effectuer ? : ")
-        return calcul
-
-
     nombre1 = demander_nombre()
     calcul = demander_calcul()
     nombre2 = demander_nombre()
@@ -42,10 +38,10 @@ while True: #permet de relancer le programe directement apres qu'il ai trouver u
     elif calcul in list_multiplication:
         resultats = nombre1 * nombre2
     elif calcul == "/":
-        if nombre2 == 0:
-            resultats = "Erreur, division par 0 impossible" #Exclusion de la division par O
-        else :
-            resultats = nombre1 / nombre2
+        while nombre2 == 0:
+            print("Erreur : Division par 0 impossible. Veuillez saisir un autre nombre.")
+            nombre2 = demander_nombre()
+        resultats = nombre1 / nombre2
     elif calcul == "//":
         resultats = nombre1 // nombre2
     elif calcul == "%":
